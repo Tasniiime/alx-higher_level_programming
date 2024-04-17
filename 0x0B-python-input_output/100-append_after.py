cbe7ddfc8b96cfde9a5a81eb2bc3d9af3e74de64
tasnime <tasnim.fr2008@gmail.com>
2024-04-17 14:58:18 -0700

    task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,22 +1,18 @@
-
 #!/usr/bin/python3
 """
-Contains the "append after" function
+define append module
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appends "new_string" after a line containing
-    "search_string" in "filename" """
-    with open(filename, 'r', encoding='utf-8') as f:
-        line_list = []
-        while True:
-            line = f.readline()
-            if line == "":
-                break
-            line_list.append(line)
+    """
+    inserts a line of text to a file
+    """
+    with open(filename, "r+", encoding="utf-8") as f:
+        s = ""
+        for line in f:
+            s += line
             if search_string in line:
-                line_list.append(new_string)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(line_list)
-
+                s += new_string
+        f.seek(0)
+        f.write(s)
