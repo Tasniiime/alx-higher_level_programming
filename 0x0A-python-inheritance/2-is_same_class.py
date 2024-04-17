6ff2ccc63ff293cddfcde0427d0d6aced830a7e5
tasnime <tasnim.fr2008@gmail.com>
2024-04-17 11:43:07 -0700

    task

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 2-is_same_class.py
-# Tasnime fersioui <tasnim.fr2008@gmail.com>
 """Defines a class-checking function."""
 
 
@@ -13,6 +11,6 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    if type(obj) is  a_class:
+    if type(obj) ==  a_class:
         return True
     return False
