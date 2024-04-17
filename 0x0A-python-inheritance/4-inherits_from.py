2080f342668d26ea55e051bc255ccff5ab9c13fb
tasnime <tasnim.fr2008@gmail.com>
2024-04-17 11:32:35 -0700

    python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from
-'''
+''' module: 4-inherits_from'''
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-    return type(obj) is not a_class and isinstance(obj, a_class)
+    '''the object is an instance of a class that inherited (directly or indirectly) obj: an object a_class: a class returns None '''
+    return type(obj) != a_class and isinstance(obj, a_class)
